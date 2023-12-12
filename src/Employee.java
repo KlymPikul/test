c15bda20b99bb170abfcd19a04170bd1866797ee
KlymPikul <klimpikul1989@gmail.com>
2023-12-12 18:17:33 +0100

    фÐadd properties and constructor

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,10 @@
 public class Employee {
+    private String firstName;
+    private String lastName;
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
 }
